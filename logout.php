5b0367060660777acbc300a9d254bee2f3a8d5d5
JoshHyde9 <joshhyde546@gmail.com>
2019-04-13 23:36:33 +1000

    Users can now logout

--- logout.php
+++ logout.php
@@ -2,11 +2,19 @@
 include 'classes/db.php';
 include 'classes/Login.php';
 
+if (!Login::isLoggedIn()) {
+    die('Not logged in!');
+}
+
 if (isset($_POST['confirm'])) {
-    if ($_POST['alldevices'] == 'alldevices') {
-
+    if (isset($_POST['alldevices'])) {
+        DB::query('DELETE FROM login_tokens WHERE user_id=:userid', array(':userid' => Login::isLoggedIn()));
     } else {
-
+        if (isset($_COOKIE['SNID'])) {
+            DB::query('DELETE FROM login_tokens WHERE token=:token', array(':token' => sha1($_COOKIE['SNID'])));
+        }
+        setcookie('SNID', '1', time() - 3600);
+        setcookie('SNID_', '1', time() - 3600);
     }
 }
 ?>
@@ -15,5 +23,5 @@ if (isset($_POST['confirm'])) {
 <p>Are you sure you want to logout of your account?</p>
 <form action="logout.php" method="post">
 <input type="checkbox" value="alldevices" name="alldevices"> Logout all devices?
-<input type="submit" name="comfirm" value="Confirm">
+<input type="submit" name="confirm" value="Confirm">
 </form>
