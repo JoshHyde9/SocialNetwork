3325c080b3a77ca5741881a6c7cfd8aa70871ffa
JoshHyde9 <joshhyde546@gmail.com>
2019-04-14 00:50:40 +1000

    Created Forgot Password form

---
+++ forgot-password.php
@@ -0,0 +1,9 @@
+<?php
+include 'classes/db.php';
+?>
+
+<h1>Forgot Password?</h1>
+<form action="forgot-password.php" method="post">
+    <input type="text" name="email" placeholder="Email...">
+    <input type="submit" value="forgotpassword" value="Reset Password">
+</form>
