2cd37aff6e84a1dca4e23a03eceb91c719127b15
JoshHyde9 <joshhyde546@gmail.com>
2019-04-14 00:51:39 +1000

    Fixed form

--- forgot-password.php
+++ forgot-password.php
@@ -5,5 +5,5 @@ include 'classes/db.php';
 <h1>Forgot Password?</h1>
 <form action="forgot-password.php" method="post">
     <input type="text" name="email" placeholder="Email...">
-    <input type="submit" value="forgotpassword" value="Reset Password">
+    <input type="submit" name="forgotpassword" value="Reset Password">
 </form>
