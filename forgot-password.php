b114516ab07ecb679327576b79ae15ec8d2ad35a
JoshHyde9 <joshhyde546@gmail.com>
2019-04-14 02:10:21 +1000

    Added change password mail service

--- forgot-password.php
+++ forgot-password.php
@@ -1,5 +1,17 @@
 <?php
 include 'classes/db.php';
+
+if (isset($_POST['forgotpassword'])) {
+    $cstrong = true;
+    $token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
+
+    $email = $_POST['email'];
+    $user_id = DB::query('SELECT id FROM users WHERE email=:email', array(':email' => $email))[0]['id'];
+    DB::query('INSERT INTO password_tokens VALUES (null, :token, :user_id)', array(':token' => sha1($token), ':user_id' => $user_id));
+    echo 'Email sent!';
+    echo '<br />';
+    echo $token;
+}
 ?>
 
 <h1>Forgot Password?</h1>
