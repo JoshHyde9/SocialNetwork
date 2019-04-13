f7121bc7c5d90a80ad9668addc1abb21758bcbd3
JoshHyde9 <joshhyde546@gmail.com>
2019-04-13 23:17:48 +1000

    Made login class

--- logout.php
+++ logout.php
@@ -1,10 +1,19 @@
 <?php
 include 'classes/db.php';
+include 'classes/Login.php';
+
+if (isset($_POST['confirm'])) {
+    if ($_POST['alldevices'] == 'alldevices') {
+
+    } else {
+
+    }
+}
 ?>
 
 <h1>Logout of your account</h1>
 <p>Are you sure you want to logout of your account?</p>
 <form action="logout.php" method="post">
-<input type="checkbox" name="alldevices"> Logout all devices?
+<input type="checkbox" value="alldevices" name="alldevices"> Logout all devices?
 <input type="submit" name="comfirm" value="Confirm">
 </form>
