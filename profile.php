29bcd32995313057555105c5eec5df5256530bbb
JoshHyde9 <joshhyde546@gmail.com>
2019-04-14 11:01:32 +1000

    Users acn have their accounts verified

--- profile.php
+++ profile.php
@@ -2,14 +2,62 @@
 include 'classes/db.php';
 include 'classes/Login.php';
 
-if (Login::isLoggedIn()) {
-    echo 'Logged in!';
-    echo Login::isLoggedIn();
-} else {
-    echo 'Not Logged in!';
+$username = "";
+$verified = false;
+$isFollowing = false;
+
+if (isset($_GET['username'])) {
+    if (DB::query('SELECT username FROM users WHERE username=:username', array(':username' => $_GET['username']))) {
+        $username = DB::query('SELECT username FROM users WHERE username=:username', array(':username' => $_GET['username']))[0]['username'];
+        $userid = DB::query('SELECT id FROM users WHERE username=:username', array(':username' => $_GET['username']))[0]['id'];
+        $verified = DB::query('SELECT verified FROM users WHERE username=:username', array(':username' => $_GET['username']))[0]['verified'];
+        $followerid = Login::isLoggedIn();
+
+        if (isset($_POST['follow'])) {
+            if ($userid != $followerid) {
+                if (!DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid' => $userid))) {
+                    if ($followerid == 13) {
+                        DB::query('UPDATE users SET verified=1 WHERE id=:userid', array(':userid' => $userid));
+                    }
+                    DB::query('INSERT INTO followers VALUES (null, :userid, :followerid)', array(':userid' => $userid, ':followerid' => $followerid));
+                } else {
+                    echo 'Already following!';
+                }
+                $isFollowing = true;
+            }
+        }
+        if (isset($_POST['unfollow'])) {
+
+            if ($userid != $followerid) {
+                if (DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid' => $userid))) {
+                    if ($followerid == 13) {
+                        DB::query('UPDATE users SET verified=0 WHERE id=:userid', array(':userid' => $userid));
+                    }
+                    DB::query('DELETE FROM followers WHERE user_id=:userid AND follower_id=:followerid', array(':userid' => $userid, ':followerid' => $followerid));
+                }
+                $isFollowing = false;
+            }
+        }
+        if (DB::query('SELECT follower_id FROM followers WHERE user_id=:userid', array(':userid' => $userid))) {
+            $isFollowing = true;
+        }
+    } else {
+        die('User not found!');
+    }
 }
 ?>
 
-<title>Profile</title>
+<title><?php echo $username; ?>'s Profile</title>
 
-<h1>Profile</h1>
+<h1><?php echo $username; ?>'s Profile<?php if ($verified) {echo ' - Verified';}?></h1>
+<form action="profile.php?username=<?php echo $username; ?>" method="post">
+<?php
+if ($userid != $followerid) {
+    if ($isFollowing) {
+        echo '<input type="submit" name="unfollow" value="Unfollow">';
+    } else {
+        echo '<input type="submit" name="follow" value="Follow">';
+    }
+}
+?>
+</form>
