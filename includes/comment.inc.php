6f8dd6a72aaac1fe5da3850003e8d20148d25bee
JoshHyde9 <joshhyde546@gmail.com>
2019-04-15 12:36:19 +1000

    Users can view comments

--- includes/comment.inc.php
+++ includes/comment.inc.php
@@ -7,10 +7,20 @@ class Comment
             die('Comment must be 200 characters long or less!');
         }
 
-        if (DB::query('SELECT id FROM posts WHERE id=:postid', array(':postid' => $userId))) {
+        if (!DB::query('SELECT id FROM posts WHERE id=:postid', array(':postid' => $postId))) {
             echo 'Invalid post ID';
         } else {
-            DB::query('INSERT INTO comments VALUES (null, :comment, :userid, NOW(), :postid', array(':comment' => $commentBody, ':userid' => $userId, ':postid' => $postId));
+            DB::query('INSERT INTO comments VALUES (\'\', :comment, :userid, NOW(), :postid', array(':comment' => $commentBody, ':userid' => $userId, ':postid' => $postId));
+        }
+    }
+
+    public static function displayComments($postId)
+    {
+        $comments = DB::query('SELECT comments.comment, users.username FROM comments, users
+        WHERE post_id = 11
+        AND comments.user_id = users.id', array(':postid' => $postId));
+        foreach ($comments as $comment) {
+            echo $comment['comment'] . " ~ " . $comment['username'] . "<hr />";
         }
     }
 }
--- index.php
+++ index.php
@@ -16,29 +16,31 @@ if (isset($_GET['postid'])) {
     Post::likePost($_GET['postid'], $userid);
 }
 
-if (isset($_GET['comment'])) {
+if (isset($_POST['comment'])) {
     Comment::createComment($_POST['commentbody'], $_GET['postid'], $userid);
 }
 
-$followingPosts = DB::query('SELECT posts.id, posts.body, posts.likes, users.username FROM users, posts, followers
+$followingPosts = DB::query('SELECT posts.id, posts.body, posts.likes, users.`username` FROM users, posts, followers
 WHERE posts.user_id = followers.user_id
 AND users.id = posts.user_id
-AND follower_id = 13
-ORDER BY posts.likes DESC;');
+AND follower_id = :userid
+ORDER BY posts.likes DESC;', array(':userid' => $userid));
 
 foreach ($followingPosts as $post) {
-    echo $post['body'] . " ~ " . $post['username'] . "";
-    echo "<form action='index.php?&postid=" . $post['id'] . "' method='post'>";
+    echo $post['body'] . " ~ " . $post['username'];
+    echo "<form action='index.php?postid=" . $post['id'] . "' method='post'>";
     if (!DB::query('SELECT post_id FROM post_likes WHERE post_id=:postid AND user_id=:userid', array(':postid' => $post['id'], ':userid' => $userid))) {
         echo "<input type='submit' name='like' value='Like'>";
     } else {
         echo "<input type='submit' name='unlike' value='Unlike'>";
     }
-    echo "<span>" . $post['likes'] . " Likes</span>
-        <form action='index.php?postid=" . $post['id'] . "' method='post'>
-        <textarea name='commentbody' cols='50' rows='3'></textarea>
-            <input type='submit' name='comment' value='Comment'>
+    echo "<span>" . $post['likes'] . " likes</span>
         </form>
-    </form>
-    <hr /> <br />";
+        <form action='index.php?postid=" . $post['id'] . "' method='post'>
+        <textarea name='commentbody' rows='3' cols='50'></textarea>
+        <input type='submit' name='comment' value='Comment'>
+        </form>";
+    Comment::displayComments($post['id']);
+    echo "
+        <hr /></br />";
 }
