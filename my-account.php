5a337e40e4c2ee69bdbf45ae32552f0779e4ae7d
JoshHyde9 <joshhyde546@gmail.com>
2019-04-15 13:42:46 +1000

    Uploaded profile image is now stored in database

--- my-account.php
+++ my-account.php
@@ -1,4 +1,14 @@
 <?php
+include 'includes/db.inc.php';
+include 'includes/login.inc.php';
+
+$showTimeline = false;
+
+if (Login::isLoggedIn()) {
+    $userid = Login::isLoggedIn();
+} else {
+    echo due('Not logged in!');
+}
 
 if (isset($_POST['uploadprofileimg'])) {
     $image = base64_encode(file_get_contents($_FILES['profileimg']['tmp_name']));
@@ -11,7 +21,14 @@ if (isset($_POST['uploadprofileimg'])) {
     $context = stream_context_create($options);
     $imgurURL = "https://api.imgur.com/3/image";
 
+    if ($_FILES['profileimg']['size'] > 1048576) {
+        die('Image is too big!, must be 10MB or less');
+    }
+
     $res = file_get_contents($imgurURL, false, $context);
+    $res = json_decode($res);
+
+    DB::query("UPDATE users SET profileimg = :profileimg WHERE id=:userid", array(':profileimg' => $res->data->link, ':userid' => $userid));
 }
 ?>
 
